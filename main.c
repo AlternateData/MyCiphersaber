d6c9ed83aefe0238daa6cbf35e92ac752cf52da3
Louis Kronberg <louiskronberg@gmail.com>
2019-02-27 08:46:35 +0100

    implementet untested version off arccfour

--- main.c
+++ main.c
@@ -1,12 +1,81 @@
 /* My own implementation of the CipherSaber algorithm
  * Author: Louis Kronberg
- *
+ * Date: 26.02.2019
  */
 #include <stdio.h>
 #include <stdlib.h>
+
+
 #define BUFF_LEN 256
-char sbox[BUFF_LEN] = {0};
+#define IV_LEN 10
+
+
 char key[BUFF_LEN] = {0};
+/* These correspond to the i and j counters mentioned in the specs.
+ * They are used to generate random bytes.
+ */
+int counter_i;
+int counter_j;
+
+void swap(unsigned char * lst, int i, int j){
+  unsigned char tmp = lst[i];
+  lst[i] = lst[j];
+  lst[j] = tmp;
+}
+
+void init_sbox(unsigned char *sbox, const unsigned char * key){
+  for(int i = 0; i < BUFF_LEN; i++){
+    sbox[i] = i;
+  } 
+
+  unsigned  char j = 0;
+  for(int i = 0; i < BUFF_LEN; i++){
+    j = (j + sbox[i] + key[i]);
+    swap(sbox, i, j);
+  }
+}
+
+void init_key(FILE * key_file, unsigned char * iv, unsigned char * key){
+
+  /* load key from file into buffer*/
+  char c;
+  int i = 0;
+  for(;i < IV_LEN; i++){
+    key[i]  = iv[i];
+  }
+
+  while((c = fgetc(key_file)) != EOF && i < BUFF_LEN){
+    key[i] = c;
+    i++;
+  }
+  
+  /* fill up the remaining space with the key */
+  for(int j = i; j < BUFF_LEN; j++){
+    key[j] = key[j % i];
+  }
+
+  //printf("%s\n", key);
+
+}
+
+void arcfour(char * in, int in_len, const unsigned char * key, char * out){
+  unsigned char sbox[BUFF_LEN] = {0};
+  init_sbox(sbox, key);
+  unsigned char i = 0;
+  unsigned char j = 0;
+  unsigned char t = 0;
+  for(int k = 0; k < in_len; k++){
+    i++; 
+    j += sbox[i];
+    swap(sbox, i, j);
+    t = sbox[i] + sbox[j];
+    out[k] = in[k] ^ sbox[t];
+  }
+
+}
+
+char* encrypt(FILE*);
+char* decrypt(FILE*);
 
 int main(int argc, char *argv[]){
   if(argc != 3){
@@ -34,25 +103,7 @@ int main(int argc, char *argv[]){
     exit(1);
   }
 
-  /* init key */
-  char c;
-  int i = 0;
-  while((c = fgetc(key_file)) != EOF && i < BUFF_LEN){
-    key[i] = c;
-    i++;
-  }
-  for(int j =i; i< BUFF_LEN; i++){
-    key[j] = key[j % i];
-  }
-
-  //printf("%s\n", key);
   
-  /* init S-Box */
-  for(int i = 0; i < BUFF_LEN; i++){
-    sbox[i] = i;
-  } 
-
-
 
   fclose(ptext_file); 
   fclose(key_file); 
