3951e9477c47bcd15dad62aa5d95dcee37d0b229
Louis Kronberg <louiskronberg@gmail.com>
2019-03-13 20:11:07 +0100

    finish up main

--- main.c
+++ main.c
@@ -9,19 +9,23 @@
 
 #define OPT_STR  "edli:"
 
-char* prompt();
 void  print_help(int code);
 
 
 char * progname = NULL;
+int   niter;
+char* infname;
+char  mode;
+char* extension;
+
+
 
 int main(int argc, char * argv[]){
   int opt;
 
-  int   niter   = C2_KEY_ITER;
-  char* infname = "";
-  char  mode    = ' ';
-  char* ext     = C2_FILE_EXT;
+  niter   = C2_KEY_ITER;
+  mode    = ' ';
+  extension     = C2_FILE_EXT;
 
   FILE* in    = NULL;
   FILE* out   = NULL;
@@ -32,7 +36,8 @@ int main(int argc, char * argv[]){
     switch(opt){
     case 'l':
       niter = C1_KEY_ITER;
-      ext = C1_FILE_EXT;
+      extension = C1_FILE_EXT;
+      fprintf(stderr, "[WARNING]: Vulnerable Ciphersaber1 algorithm selected. Please use Ciphersaber2 instead!\n");
       break;
     case 'e':
       mode = 'e';
@@ -53,14 +58,17 @@ int main(int argc, char * argv[]){
     }
   }
 
- 
+  if(mode == ' '){
+    fprintf(stderr, "[ERROR]: Neither -d nor -e was given as an option. These are essential.\n");
+    print_help(EXIT_FAILURE);
+  }
+
 
   if(!in){
     fprintf(stderr, "[ERROR]: No file was given to en/decrypt.\n");
     print_help(EXIT_FAILURE);
   }
 
-  char * secret = getpass(PROMPT);
 
   /* get file length, prepare memory and read from in */
   fseek(in, 0, SEEK_END);
@@ -70,42 +78,49 @@ int main(int argc, char * argv[]){
 
   if(!msg){
     fprintf(stderr, "[ERROR]: Failed to allocate memory for message.\n");
+    exit(EXIT_FAILURE);
   }
 
   fread(msg, sizeof(char), fsize, in);
 
+  char* secret = getpass(PROMPT);
+  char* cipher = NULL;
 
-  /* warn in case vulnerable ciphersaber1 was selected */
-  if(niter == C1_KEY_ITER){
-    fprintf(stderr, "[WARNING]: Vulnerable Ciphersaber1 algorithm selected. Please use Ciphersaber2 instead!\n");
-  }
-
-  char *cipher = NULL;
+  char *outfname;
   if(mode=='e'){
-    printf("Encrypting!\n");
     cipher = encrypt(msg, fsize, secret, niter);
+
+    outfname = malloc(sizeof(char) * (strlen(infname) + strlen(extension)));
+    strcat(outfname, infname);
+    strcat(outfname, extension);
   }else if(mode=='d'){
-    printf("Decrypting!\n");
     cipher = decrypt(msg, fsize, secret, niter);
-  }else if(mode == ' '){
-    fprintf(stderr, "[ERROR]: Neither -d nor -e was given as an option.\n");
-    free(msg);
-    print_help(EXIT_FAILURE);
+
+    outfname = malloc(strlen(infname) - strlen(extension));
+    memcpy(outfname, infname, strlen(infname) - strlen(extension));
+  }
+
+  if(!cipher){
+    fprintf(stderr, "[ERROR]: Internal method %s() returned NULL\n", mode=='e' ? "encrypt" : "decrypt");
+    exit(EXIT_FAILURE);
   }
 
 
-  /* open out file for writing */
-  char outfname[strlen(infname) + strlen(ext)];
-  strcat(outfname, infname);
-  strcat(outfname, ext);
   out = fopen(outfname, "wb");
-
   if(!out){
     fprintf(stderr, "[ERROR]: Could not open output file %s\n", outfname);
     exit(EXIT_FAILURE);
   }
 
+  fwrite(cipher, 1, fsize, out);
+
+
+  /* clean up */
+  fclose(in);
+  fclose(out);
   free(msg);
+
+  fclose(in);
   if(cipher){
     free(cipher);
   }
